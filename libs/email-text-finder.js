7340bd2ae5542297c6bf20ca8e6e02362f926cde
Sandro Munda <sandro@munda.me>
2014-09-06 15:46:54 +0200

    Supported the 'web' source type.

--- libs/email-text-finder.js
+++ libs/email-text-finder.js
@@ -1,20 +1,35 @@
 'use strict';
 
-function Finder() {
+var async = require('async');
+var cheerio = require('cheerio');
+
+function Finder(opts) {
   var emailRegexp = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/;
 
   this.getEmails = function (text, callback) {
-    var emails = [];
+    async.waterfall([
+      function (cb) {
+        if (opts.sourceType === 'web') {
+          var $ = cheerio.load(text);
+          cb(null, $('body').text());
+        } else {
+          cb(null, text);
+        }
+      },
+      function (text, cb) {
+        var emails = [];
 
-    var match = emailRegexp.exec(text);
-    while (match !== null) {
-      emails.push(match[0]);
-      text = text.replace(new RegExp(match[0], 'gi'), '');
-      match = emailRegexp.exec(text);
-    }
+        var match = emailRegexp.exec(text);
+        while (match !== null) {
+          emails.push(match[0]);
+          text = text.replace(new RegExp(match[0], 'gi'), '');
+          match = emailRegexp.exec(text);
+        }
 
-    callback(null, emails);
+        cb(null, emails);
+      }
+    ], callback);
   };
 }
 
-module.exports = new Finder();
+module.exports = Finder;
